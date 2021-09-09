f484c6194830ae2cb457bd0ec792f2d14b392898
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-09 11:07:48 +0300

    scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import App from './Components/App';
+import './index.scss';
 
 ReactDOM.render(
     <App />,
